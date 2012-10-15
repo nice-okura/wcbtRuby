d0ab24ae3e76e3f53f605c1c5e29f98d3b8a3a50
fujitani <fujitani@ertl.jp>
2012-10-15 20:29:46 +0900

    add *forCygwin

---
+++ util/randomSchesimFile_forCygwin.rb
@@ -0,0 +1,75 @@
+#!/usr/bin/ruby
+# -*- coding: utf-8 -*-
+#
+#=Author: fujitani
+#=Date: 2012/10/09
+#
+# rtOutput.rb
+#
+# ランダムなタスクセットを生成し，schesimフォルダにタスクセットファイル(.rb, .json)を生成する
+#
+# Usage: 
+# % ruby randomSchesimFile.rb [結果出力ファイル名] [タスク数] 
+require "./manager"
+require "./export_schesim"
+require "fileutils"
+
+#cd ../schesim-0.7.2; ruby schesim.rb -t tmp/tmp.json -d tmp/tmp.rb -r tmp/tmp.res -e 10 ; cd ../wcbtRuby/
+# マクロ
+SCHESIM_FOLDER = "/cygdrive/c/Users/fujitani/Documents/schesim-0.7.2/"
+@manager = AllManager.new
+output_filename = ARGV[0]
+
+
+def make_taskset(t_count, r_count, g_count, req_count, range, require_range, offset_range)
+  # タスクセット生成
+  info = { }
+  info[:proc_num] = 2
+  info[:mode] = CREATE_MANUALLY
+  info[:extime_range] = range
+  info[:assign_mode] = ID_ORDER  # ID順
+  info[:require_count] = req_count
+  info[:require_range] = require_range # CS範囲
+  info[:priority_mode] = PRIORITY_BY_UTIL # タスク使用率順にタスクIDと優先度をつける
+  info[:period_range] = range.first*5..range.last*5
+  info[:offset_range] = offset_range
+
+  @manager.create_tasks(t_count, r_count, g_count, info)
+
+  @manager.save_tasks("tmp_#{t_count}")
+end
+
+taskset_count = 1   # タスクセット数
+t_count = ARGV[1].to_i   # タスク数
+r_count = t_count*2         # リソース要求数
+g_count = t_count/2         # グループ数
+req_count = 2       # タスク当たりのリソース要求数
+ex_range = 50..100  # タスク実行時間の範囲
+req_range = 2..5   # CS範囲
+offset_range = 0..50
+
+# main
+if ARGV.size != 2
+  puts "引数が不正"
+  exit
+end
+
+taskset_count.times do |i|
+  # タスクセット生成
+  make_taskset(t_count, r_count, g_count, req_count, ex_range, req_range, offset_range)
+
+  # json出力
+  Dir::mkdir(output_filename) unless File::exist?(output_filename)
+  exp = EXPORT_SCHESIM.new("#{output_filename}/#{output_filename}_schesim")
+  exp.output(@manager)
+
+  # schesimフォルダにコピー
+  FileUtils.copy_entry(output_filename, "#{SCHESIM_FOLDER}#{output_filename}_schesim")
+
+  wcrt_array = []
+  1.upto(@manager.tm.get_task_array.size) do |id|
+    wcrt_array << TaskManager.get_task(id).wcrt*10
+  end
+
+  puts wcrt_array.to_s.gsub(/\[|\]|\"/, "").gsub(/, /, ",")
+end
---
+++ util/rtOutputRandomTaskset_forWindows.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+#
+#=Author: fujitani
+#=Date: 2012/10/09
+#
+# rtOutpuRandomTaskset.sh
+#
+# ランダムなタスクセットを生成し，平均，最大実実行時間を計測する
+#
+# Usage: 
+# % sh ./rtOutputRandomTaskset_forCygwin.sh [結果出力ファイル名] [タスクセット数]
+
+readonly SCHESIM_FOLDER="/cygdrive/c/Users/fujitani/Documents/schesim-0.7.2/"
+readonly WCBT_FOLDER="/cygdrive/c/Users/fujitani/Documents/wcbtRuby/"
+readonly TMP_FILENAME="tmp"
+
+# タスクパラメータ
+readonly TASKSET=$2
+readonly CPU=2
+readonly TASK=4
+readonly MAX_TASK=8
+readonly REQUIRE=8
+readonly RESOURCE=2
+readonly REQ_COUNT=2
+
+if [ $# -eq 2 ]; then
+    for tsk in `seq ${TASK} 2 ${MAX_TASK}`; do
+	# 最大応答時間を格納する配列
+	WCRT=()
+
+	# ファイル新規作成
+	cd ${SCHESIM_FOLDER}
+	mkdir ${TMP_FILENAME}_schesim/data_${tsk}task/ >& /dev/null
+	filename="${TMP_FILENAME}_schesim/data_${tsk}task/task_wcrt_${tsk}task.txt"
+	touch ${filename}
+	echo > ${filename}
+	
+	for i in `seq 1 ${TASKSET}`; do
+	    echo ${i}
+	    cd ${WCBT_FOLDER}
+	    #ruby ./util/randomSchesimFile.rb ${TMP_FILENAME} ${tsk}
+	    WCRT=("${WCRT[@]}" `ruby ./util/randomSchesimFile_forCygwin.rb ${TMP_FILENAME} ${tsk}`)
+            
+	    cd ${SCHESIM_FOLDER}
+	    ./auto_schesim_forCygwin.sh ./tmp_schesim/tmp_schesim >& /dev/null
+	    
+	    ruby utils/stats.rb -c -i ${TMP_FILENAME}_schesim/${TMP_FILENAME}_schesim.log -o /dev/null > ${TMP_FILENAME}_schesim/data_${tsk}task/${TMP_FILENAME}_schesim_${i}.csv
+	done
+	cd ${SCHESIM_FOLDER}
+	#echo ${filename}
+	for i in `seq 1 ${TASKSET}`; do
+	    echo ${WCRT[i-1]} >> ${filename}
+	done
+    done
+    cd ${WCBT_FOLDER}
+    ruby ${WCBT_FOLDER}util/csv2plt.rb ${SCHESIM_FOLDER}${TMP_FILENAME}_schesim/ ${WCBT_FOLDER}test.txt ${MAX_TASK}
+else
+    echo "Usage: \n% ./rtOutputRandomTaskset.sh [結果出力ファイル名] [タスクセット数]"
+fi
