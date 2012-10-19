cf17d46b1789124f5172263a77b198e4a3c49ae1
fujitani <fujitani@ertl.jp>
2012-10-19 14:22:55 +0900

    modify util/rtOutputRandomTaskset.sh to compute the difference between max_rt and wcrt

--- util/csv2plt.rb
+++ util/csv2plt.rb
@@ -14,17 +14,22 @@ require "pp"
 DIRNAME = ARGV[0] # 入力データのあるフォルダ 末尾の"/"あり
 OUTPUT_FILE = ARGV[1]
 MAX_TASK = ARGV[2].to_i
-$task_stats_data = { }
 
+$task_stats_data = { }
+# task_stats_data[tsk][id][0]: 平均応答時間の平均
+# task_stats_data[tsk][id][1]: 最大実応答時間の平均
+# task_stats_data[tsk][id][2]: 最大応答時間の平均
+# task_stats_data[tsk][id][3]: 最大実応答時間と最大応答時間の差
 
 task_count = []
 4.step(MAX_TASK, 2){ |i| task_count << i }
 
 task_count.each do |tsk|
   $task_stats_data[tsk] = { }
-
   rt_ave = Array.new(tsk, 0.0) # 平均応答時間
   rt_wc = Array.new(tsk, 0.0)  # 最大実応答時間
+  rt_diff = Hash.new{ |hash, id| hash[id] = []} # 最大実応答時間と最大応答時間の差
+  
   sets = 0     # タスクセット数
   
   Dir::glob(DIRNAME+"data_#{tsk}task/*.csv").each do |filename|
@@ -37,13 +42,19 @@ task_count.each do |tsk|
         $task_stats_data[tsk][id] = [] if $task_stats_data[tsk][id] == nil
         rt_ave[id-1] += row[2].to_f # 平均応答時間
         rt_wc[id-1] += row[3].to_f  # 最大実応答時間"
-      end
-      tsk.times do |id|
-        $task_stats_data[tsk][id+1] = [rt_ave[id]/sets, rt_wc[id]/sets]
+        # Ex. rt_diff[Taskid] = [タスクセット1の最大実応答時間, ...]
+        # rt_diff[1] = [100.0, 200.0, 341.0, 134.0]
+        # rt_diff[2] = [134.0, 462.0, 456.0, 235.0]...
+        rt_diff[id] << row[3].to_f # タスクセット毎に最大実応答時間を格納
       end
     end
   end
+  # 全ファイル走査した後，$task_stats_dataに平均を代入
+  tsk.times do |id|
+    $task_stats_data[tsk][id+1] = [rt_ave[id]/sets, rt_wc[id]/sets]
+  end
 
+  # 計算によって求まった最大応答時間の平均
   Dir::glob(DIRNAME+"data_#{tsk}task/task_wcrt_#{tsk}task.txt").each do |filename|
     sets = 0  # タスクセット数
     ret = Array.new(tsk, 0.0)
@@ -53,7 +64,12 @@ task_count.each do |tsk|
 
       while l = fp.gets
         sets += 1
-        l.split(",").each_with_index { |v, i| ret[i] += v.to_f }
+        wcrt = l.split(',')
+        wcrt.each_with_index do |v, idx|
+          ret[idx] += v.to_f 
+          #puts "#{v.to_f} - #{rt_diff[idx+1][sets-1]}"
+          rt_diff[idx+1][sets-1] = v.to_f - rt_diff[idx+1][sets-1]
+        end
       end
 
       ret.each_with_index do |v, id|
@@ -61,8 +77,10 @@ task_count.each do |tsk|
       end
     end
   end
+  pp rt_diff
 end  
 pp $task_stats_data
+
 
 # TASKID:1 の平均応答時間と最大実応答時間
 
